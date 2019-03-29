f84a62f3d523ef99a300b71eae753c38c712ab81
rachartier <raphael.chartier@etu.uca.fr>
2019-03-29 19:31:15 +0100

    changement fichier preuve et les fichiers d editions sont maintenant cachés

--- code/copy_apk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-
-cp /home/rachartier/Documents/cours/android/projet/androidplateformgame/code/Dynamo/app/release/app-release.apk dynamo.apk
-cp /home/rachartier/Documents/cours/android/projet/androidplateformgame/code/Dynamo/app/release/app.aab dynamo.aab
